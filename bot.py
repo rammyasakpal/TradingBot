9844d1701f46eca7278c62b8248c540c5a17e8b2
Krish Sanghvi <124244390+Krishsanghvii@users.noreply.github.com>
2023-09-01 14:10:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 def get_crypto_trading(api_key, api_secret):
     exchange = ccxt.binance({
-        'xsDDXeMWlxSzJfTsElDAJquwy7HfYiybymafddyDLUrszrAuvF4vg37kqPorM2dQ': api_key,
+        'apiKey': api_key,
         'secret': api_secret,
     })
 
